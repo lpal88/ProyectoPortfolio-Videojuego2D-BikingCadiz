7138b0379760592ee3fc81ba212cb271c7e25d3d
lpal88 <lpalpar893@g.educaand.es>
2023-03-05 13:39:23 +0100

    tabs

---
+++ dist/index.a5fb9f2e.js
@@ -0,0 +1,28 @@
+"use strict";
+const li = document.querySelectorAll(".li");
+const caja = document.querySelectorAll(".caja");
+// CLICK en li
+// TODOS .li quitar la clase activo
+// TODOS .caja quitar la clase activo
+// .li con la posicion se añadimos la clase activo
+// .caja con la posicion se añadimos la clase activo
+// Recorriendo todos los LI
+li.forEach((cadaLi, i)=>{
+    // Asignando un CLICK a CADALI
+    li[i].addEventListener("click", ()=>{
+        console.log("clicando");
+        // Recorrer TODOS los .li
+        li.forEach((cadaLi, i)=>{
+            // Quitando la clase activo de cada li
+            li[i].classList.remove("activo");
+            // Quitando la clase activo de cada caja
+            caja[i].classList.remove("activo");
+        });
+        // En el li que hemos click le añadimos la clase activo
+        li[i].classList.add("activo");
+        // En el caja con la misma posición le añadimos la clase activo
+        caja[i].classList.add("activo");
+    });
+});
+
+//# sourceMappingURL=index.a5fb9f2e.js.map
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,5 +1,3 @@
-
-
 'use strict'
 
 const bloque    = document.querySelectorAll('.bloque')
